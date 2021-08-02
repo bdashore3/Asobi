e936992e206d7ef0576e45590f6c13a4fe76c51f
kingbri <bdashore3@gmail.com>
2021-08-02 13:04:24 -0400

    Initial Commit

---
+++ Cubari/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cubari
+//
+//  Created by Brian Dashore on 8/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cubari/CubariApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubariApp.swift
+//  Cubari
+//
+//  Created by Brian Dashore on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct CubariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
