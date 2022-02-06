e9e8891b89fe141b0546adbeb816f3e790ee9b90
kingbri <bdashore3@gmail.com>
2022-02-06 14:48:06 -0500

    WebView: Fix duplicate pasting on MacCatalyst
    MacCatalyst has a bug where pasting text gets duplicated.
    
    Add a JS script which overrides the default paste method and makes
    sure pasting is only done once since the MacCatalyst bug also calls
    the JavaScript paste event twice.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- Asobi/WebViewModel.swift
+++ Asobi/WebViewModel.swift
@@ -96,7 +96,42 @@ class WebViewModel: ObservableObject {
         }
         """
 
-        if UIDevice.current.deviceType == .phone || UIDevice.current.deviceType == .pad {
+        let pasteJs = """
+        const inputs = document.querySelectorAll("input[type=text]")
+        let alreadyPasted = false
+
+        for (const input of inputs) {
+          input.addEventListener("paste", (event) => {
+            event.preventDefault()
+
+            // Don't call paste event two times in a single paste command
+            if (alreadyPasted) {
+              alreadyPasted = false
+              return
+            }
+
+            const paste = (event.clipboardData || window.clipboardData).getData("text")
+
+            const beginningString =
+              input.value.substring(0, input.selectionStart) + paste
+
+            input.value =
+              beginningString +
+              input.value.substring(input.selectionEnd, input.value.length)
+
+            alreadyPasted = true
+
+            input.setSelectionRange(beginningString.length, beginningString.length)
+
+            input.scrollLeft = input.scrollWidth
+          })
+        }
+        """
+
+        if UIDevice.current.deviceType == .mac {
+            let pasteEvent = WKUserScript(source: pasteJs, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
+            config.userContentController.addUserScript(pasteEvent)
+        } else {
             let zoomEvent = WKUserScript(source: zoomJs, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
             config.userContentController.addUserScript(zoomEvent)
         }
