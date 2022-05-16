30498bae209d1b9d2834bced5df6c2b0fc0355c2
kingbri <bdashore3@gmail.com>
2022-05-15 22:51:42 -0400

    iCloud: Add preference switch and fixes
    A toggle for syncing has been added. When the switch is toggled to on,
    any data is pushed to iCloud and handled there.
    
    The toggle currently requires an app restart due to crashes.
    
    This also fixes how history is presented with iCloud syncing since
    some history entries may be split and will show weird results.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- Asobi/DataManagement/PersistenceController.swift
+++ Asobi/DataManagement/PersistenceController.swift
@@ -30,25 +30,34 @@ struct PersistenceController {
 
     // Coredata load
     init(inMemory: Bool = false) {
+        let iCloudEnabled = UserDefaults.standard.bool(forKey: "iCloudEnabled")
+
         container = NSPersistentCloudKitContainer(name: "AsobiDB")
 
         if inMemory {
             container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
         }
-        
+
         guard let description = container.persistentStoreDescriptions.first else {
             fatalError("CoreData: Failed to find a persistent store description")
         }
+
+        if !iCloudEnabled {
+            description.cloudKitContainerOptions = nil
+        }
+
         description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
         description.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
 
         container.viewContext.automaticallyMergesChangesFromParent = true
         container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        try? container.viewContext.setQueryGenerationFrom(.current)
 
         backgroundContext = container.newBackgroundContext()
         backgroundContext.automaticallyMergesChangesFromParent = true
         backgroundContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-        
+        try? backgroundContext.setQueryGenerationFrom(.current)
+
         container.loadPersistentStores { _, error in
             if let error = error {
                 fatalError("Error: \(error.localizedDescription)")
---
+++ Asobi/Views/SettingsViews/SettingsSyncView.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsSyncView.swift
+//  Asobi
+//
+//  Created by Brian Dashore on 5/14/22.
+//
+
+import SwiftUI
+
+struct SettingsSyncView: View {
+    @AppStorage("iCloudEnabled") var iCloudEnabled = false
+
+    @State private var showiCloudAlert = false
+    
+    var body: some View {
+        Section(header: Text("Sync options"), footer: Text("iCloud syncing is currently experimental! You may have duplicates in history or bookmarks")) {
+            Toggle(isOn: $iCloudEnabled) {
+                Text("iCloud sync")
+            }
+            .onChange(of: iCloudEnabled) { _ in
+                showiCloudAlert.toggle()
+            }
+            .alert(isPresented: $showiCloudAlert) {
+                Alert(
+                    title: Text(iCloudEnabled ? "Syncing enabled" : "Syncing disabled"),
+                    message: Text("Changing this setting requires an app restart"),
+                    dismissButton: .cancel(Text("OK!"))
+                )
+            }
+        }
+    }
+}
+
+struct SettingsSyncView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsSyncView()
+    }
+}
