2934f85558d81c7e91b4377350138ba4684529c4
kingbri <bdashore3@gmail.com>
2022-06-07 00:54:13 -0400

    Asobi: Switch to a NavigationStack
    Since NavigationStack is not backwards compatible, a wrapper view is
    required (thanks Mantton for the help).
    
    A NavigationPath has to be implemented for the background
    EditBookmarkView NavigationLink due to deprecation of isActive.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- Asobi/Views/EditBookmarkView.swift
+++ Asobi/Views/EditBookmarkView.swift
@@ -19,90 +19,90 @@ struct EditBookmarkView: View {
 
     @Binding var bookmark: Bookmark?
 
+    @State var calledFromLibrary = false
     @State private var bookmarkName = ""
     @State private var bookmarkUrl = ""
     @State private var showUrlError = false
-
+    
     var body: some View {
-        NavigationView {
-            Form {
-                // Because onAppear doesn't work properly with sheet + form
-                Section {
-                    TextField("Enter title", text: $bookmarkName)
-                        .clearButtonMode(.whileEditing)
+        Form {
+            // Because onAppear doesn't work properly with sheet + form
+            Section {
+                TextField("Enter title", text: $bookmarkName)
+                    .clearButtonMode(.whileEditing)
 
-                    TextField("Enter URL", text: $bookmarkUrl)
-                        .clearButtonMode(.whileEditing)
-                        .disableAutocorrection(true)
-                        .keyboardType(.URL)
-                        .autocapitalization(.none)
-                }
-                .onAppear {
-                    bookmarkName = bookmark?.name ?? webModel.webView.title ?? ""
-                    bookmarkUrl = bookmark?.url ?? webModel.webView.url?.absoluteString ?? ""
+                TextField("Enter URL", text: $bookmarkUrl)
+                    .clearButtonMode(.whileEditing)
+                    .disableAutocorrection(true)
+                    .keyboardType(.URL)
+                    .autocapitalization(.none)
+            }
+            .onAppear {
+                bookmarkName = bookmark?.name ?? webModel.webView.title ?? ""
+                bookmarkUrl = bookmark?.url ?? webModel.webView.url?.absoluteString ?? ""
 
-                    if !(bookmarkUrl.hasPrefix("http://") || bookmarkUrl.hasPrefix("https://")) {
-                        bookmarkUrl = "https://\(bookmarkUrl)"
-                    }
+                if !(bookmarkUrl.hasPrefix("http://") || bookmarkUrl.hasPrefix("https://")) {
+                    bookmarkUrl = "https://\(bookmarkUrl)"
                 }
             }
-            .alert(isPresented: $showUrlError) {
-                Alert(
-                    title: Text("Empty fields"),
-                    message: Text("The bookmark title and URL cannot be empty. Please input the valid fields"),
-                    dismissButton: .default(Text("OK"))
-                )
-            }
-            .navigationBarTitle("Editing Bookmark", displayMode: .inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
+        }
+        .alert(isPresented: $showUrlError) {
+            Alert(
+                title: Text("Empty fields"),
+                message: Text("The bookmark title and URL cannot be empty. Please input the valid fields"),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+        .navigationBarTitle("Editing Bookmark", displayMode: .inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                if !calledFromLibrary {
                     Button("Cancel") {
                         presentationMode.wrappedValue.dismiss()
                     }
                     .keyboardShortcut(.cancelAction)
                 }
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Save") {
-                        if bookmarkUrl == "" || bookmarkName == "" {
-                            showUrlError.toggle()
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Save") {
+                    if bookmarkUrl == "" || bookmarkName == "" {
+                        showUrlError.toggle()
 
-                            return
-                        }
-
-                        if let unwrappedBookmark = bookmark {
-                            // Update an existing bookmark
-                            unwrappedBookmark.name = bookmarkName.trimmingCharacters(in: .whitespaces)
-                            unwrappedBookmark.url = bookmarkUrl.trimmingCharacters(in: .whitespaces)
-                        } else {
-                            // If called from a context menu, we need to upsert the bookmark
-                            let tempName = bookmarkName.trimmingCharacters(in: .whitespaces)
-
-                            let bookmarkRequest = Bookmark.fetchRequest()
-                            bookmarkRequest.predicate = NSPredicate(format: "name == %@", tempName)
-                            bookmarkRequest.fetchLimit = 1
-
-                            if let existingBookmark = try? backgroundContext.fetch(bookmarkRequest).first {
-                                PersistenceController.shared.delete(existingBookmark, context: backgroundContext)
-                            }
-
-                            // Set a new bookmark
-                            let bookmark = Bookmark(context: backgroundContext)
-                            bookmark.name = tempName
-                            bookmark.url = bookmarkUrl.trimmingCharacters(in: .whitespaces)
-                        }
-
-                        PersistenceController.shared.save(backgroundContext)
-
-                        presentationMode.wrappedValue.dismiss()
-
-                        bookmark = nil
+                        return
                     }
-                    .keyboardShortcut(.defaultAction)
+
+                    if let unwrappedBookmark = bookmark {
+                        // Update an existing bookmark
+                        unwrappedBookmark.name = bookmarkName.trimmingCharacters(in: .whitespaces)
+                        unwrappedBookmark.url = bookmarkUrl.trimmingCharacters(in: .whitespaces)
+                    } else {
+                        // If called from a context menu, we need to upsert the bookmark
+                        let tempName = bookmarkName.trimmingCharacters(in: .whitespaces)
+
+                        let bookmarkRequest = Bookmark.fetchRequest()
+                        bookmarkRequest.predicate = NSPredicate(format: "name == %@", tempName)
+                        bookmarkRequest.fetchLimit = 1
+
+                        if let existingBookmark = try? backgroundContext.fetch(bookmarkRequest).first {
+                            PersistenceController.shared.delete(existingBookmark, context: backgroundContext)
+                        }
+
+                        // Set a new bookmark
+                        let bookmark = Bookmark(context: backgroundContext)
+                        bookmark.name = tempName
+                        bookmark.url = bookmarkUrl.trimmingCharacters(in: .whitespaces)
+                    }
+
+                    PersistenceController.shared.save(backgroundContext)
+
+                    presentationMode.wrappedValue.dismiss()
+
+                    bookmark = nil
                 }
+                .keyboardShortcut(.defaultAction)
             }
         }
         .blur(radius: UIDevice.current.deviceType == .mac ? 0 : navModel.blurRadius)
-        .navigationViewStyle(.stack)
     }
 }
 
